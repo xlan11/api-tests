9f8fa8d6da9a31ee68bec35f1021336a026ec398
xlan11 <alankelly@gmail.com>
2023-01-11 18:11:22 +0000

    updates

---
+++ app.js
@@ -0,0 +1,5 @@
+// const apiKey = "f9a8200a794828e016db6bb8fca5a95a"
+
+// fetch(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
